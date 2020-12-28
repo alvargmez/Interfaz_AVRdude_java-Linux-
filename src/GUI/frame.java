d2e8872ccbc15f257223770a33e0cf91bb9e122a
Álvaro Gómez Sánchez <alvargmez@gmail.com>
2020-12-28 16:55:37 +0100

    añadido_icono_de_app

--- src/GUI/frame.java
+++ src/GUI/frame.java
@@ -6,6 +6,10 @@ public class frame extends JFrame {
 
     public frame(){
 
+        frameInit();  //Para poder seleccionar el icono Pudoacerlo tambien en el frame instanciado en el main()
+
+        setIconImage(new ImageIcon(getClass().getResource("Iconos/microcontrolador(64).png")).getImage()); //selecciono icono
+
         add(new panel());
 
     }
